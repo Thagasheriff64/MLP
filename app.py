de8d56125bc186a96f354f37927633e3dec783b0
Thaga Sheriff <thagasheriff105@gmail.com>
2024-08-17 14:34:20 +0530

    Machine Learning Project

--- app.py
+++ app.py
@@ -86,12 +86,12 @@ def main():
         data_df= pd.DataFrame(data,columns=column)
         st.write(data_df)
 
-        preprocessor = joblib.load(r"preprocessor\preprocessor.joblib")
-        model = joblib.load(r"models\model.joblib")
+        preprocessor = joblib.load("preprocessor\preprocessor.joblib")
+        model = joblib.load("models\model.joblib")
 
         x_test_transform = preprocessor.transform(data_df)
         pred = model.predict(x_test_transform)
-        #st.write(pred)
+        #clst.write(pred)
         st.markdown(f'<h1> Prediction for the data is : {pred} </h1>')
 if __name__  == "__main__":
     main()
